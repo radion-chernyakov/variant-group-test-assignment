3175c394a8fccad703b5c44f22db732bb1bea927
Radion Chernyakov <nixololo@gmail.com>
2024-05-09 13:29:03 +0400

    store opanai org in env 🙈

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -2,7 +2,7 @@ import OpenAI from 'openai';
 import { env } from '~/env'
 
 const client = new OpenAI({
-  organization: env.OPENAI_API_ORGANIZATION,
+  organization: env.OPENAI_ORGANIZATION,
   apiKey: env.OPENAI_API_KEY,
 });
 export default client
